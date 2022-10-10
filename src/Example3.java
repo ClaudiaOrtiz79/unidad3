280bf4c9e02e36dd4f61f326ff3b88ddde39a007
claudiaortiz79 <aluort0593@ieselcaminas.org>
2022-10-10 15:22:25 +0200

    example3

---
+++ src/Example3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Example3 {
+    public static void main(String[] args) {
+        float x;
+        float squareArea;
+        System.out.println("Enter the side");
+
+        //Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        x = inputValue.nextFloat();
+
+        squareArea = x * x ;
+        System.out.println(squareArea);
+    }
+}
