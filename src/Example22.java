e51f2aaf41108307b8fcea113f22bf3a9759d4fa
claudiaortiz79 <aluort0593@ieselcaminas.org>
2022-10-11 16:06:21 +0200

    example22

--- src/Example22.java
+++ src/Example22.java
@@ -1,11 +1,16 @@
+
 class Example22 {
     public static void main(String[] argv) {
-        int sum, product;
+        int sum, product, i;
         sum = 0;
         product = 1;
-        for (int i = 1; i <= 10; i++) {
+        i=1;
+
+        while ( i <= 10) {
             sum = sum + i;
             product = product * i;
+            i = i + 1;
+
         }
         System.out.println("Sum from 1 to 10:" + sum);
                 System.out.println("Product from 1 to 10:"+product);
